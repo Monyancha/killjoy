6aa2740f7303a4064c51a4b0cb1f10ef63143bae
nawissor <shutterman@iwanross.co.za>
2018-02-04 13:49:30 +0200

    adding images to clickable regions

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
     <h1>Killjoy - the online comunity for rental property tenants</h1></div>
   <div class="intro" id="intro">Killjoy’s main mission is to prevent landlord abuse of rental property tenants. It gives you the power to review a rental property and share your personal experiences with future tenants. Future tenants also have the option to view existing property rental reviews before making a decision on letting the property. This way we can all rent smarter. </div>
   <div class="chooser" id="chooser">
-    <div class="choosereview" id="choosereview">left</div>
+    <div class="choosereview" id="choosereview"><a href="#" title="review a rental property">Review a Rental Property</a></div>
     <div class="chooseview" id="chooseview">Right</div>
   </div>
 </div>
