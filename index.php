087573435830e6ad58c0330c97c004d0a1cef4cf
nawissor <shutterman@iwanross.co.za>
2018-02-04 14:07:18 +0200

    creating review selector

--- index.php
+++ index.php
@@ -35,8 +35,8 @@
     <h1>Killjoy - the online comunity for rental property tenants</h1></div>
   <div class="intro" id="intro">Killjoy’s main mission is to prevent landlord abuse of rental property tenants. It gives you the power to review a rental property and share your personal experiences with future tenants. Future tenants also have the option to view existing property rental reviews before making a decision on letting the property. This way we can all rent smarter. </div>
   <div class="chooser" id="chooser">
-    <div class="choosereview" id="choosereview"><a href="#" title="review a rental property">Review a Rental Property</a></div>
-    <div class="chooseview" id="chooseview">Right</div>
+    <a href="#" title="review a rental property"><div class="choosereview" id="choosereview">Review a Rental Property</div></a>
+    <div class="chooseview" id="chooseview">View rental property reviews</div>
   </div>
 </div>
 </body>
