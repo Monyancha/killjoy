c3869655914e8fbcad0bf1916b52ec115812b54e
nawissor <shutterman@iwanross.co.za>
2018-02-10 13:19:41 +0200

    if facebook user exists create session and redirect to home page

--- login-with-facebook/logindata.php
+++ login-with-facebook/logindata.php
@@ -1,10 +1,41 @@
+<?php require_once('../Connections/killjoy.php'); ?>
 <?php
 ob_start();
 if (!isset($_SESSION)) {
 session_start();
 }
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
 
-$login_seccess_url = "http://localhost/killjoy/index.php";
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+
 
 	include 'dbconfig.php';
 	//return $conn variable.
@@ -16,27 +47,39 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 		$profilePictureUrl = $_POST['profilePictureUrl'];
 		$locale = $_POST['link'];
 		
-		$result = mysql_query("SELECT COUNT(g_email) FROM social_users WHERE g_email=$email");
-	if($result === false) { 
-		die(mysql_error()); //result is false show db error and exit.
-	}
-	
-	$UserCount = mysql_fetch_array($result);
- 
-    if($UserCount[0]) //user id exist in database
-    {
-		echo 'Welcome back '.$user_name.'!';
-    }else{ //user is new
-		echo 'Hello! '.$user_name.', Thanks for Registering!';
-		@mysql_query("INSERT INTO social_users(g_name,g_email,g_id,g_image,g_link, g_active) VALUES ('".$name."','".$email."','".$fb_Id."','".$profilePictureUrl."','".$locale."','".$active."')");
-	}
+		$colname_rs_checkfbuser = "-1";
+if (isset($_POST['email'])) {
+  $colname_rs_checkfbuser = $_POST['email'];
+}
+mysql_select_db($database_killjoy, $killjoy);
+$query_rs_checkfbuser = sprintf("SELECT g_email FROM social_users WHERE g_email = %s", GetSQLValueString($colname_rs_checkfbuser, "text"));
+$rs_checkfbuser = mysql_query($query_rs_checkfbuser, $killjoy) or die(mysql_error());
+$row_rs_checkfbuser = mysql_fetch_assoc($rs_checkfbuser);
+$totalRows_rs_checkfbuser = mysql_num_rows($rs_checkfbuser);
 
-	$_SESSION['kj_username'] = $email;
-    $_SESSION['kj_authorized'] = "1";  
-	header('Location: ' . filter_var($login_seccess_url  , FILTER_SANITIZE_URL));
-		
-		
+ if($totalRows_rs_checkfbuser) //user id exist in database
+    {
+		$_SESSION['kj_username'] = $email;
+        $_SESSION['kj_authorized'] = "1"; 
+		//redirect to login page
+		header('Location: ' . filter_var($user_exists  , FILTER_SANITIZE_URL));
+    }else {
 	
+		$query = "INSERT INTO social_users(g_name,g_email,g_id,g_image,g_link, g_active) VALUES ('".$name."','".$email."','".$fb_Id."','".$profilePictureUrl."','".$locale."','".$active."')";
+		$result = mysqli_query($conn , $query) or die(mysqli_error());
+		if ($result) {
+			
+	  
+			
+				$_SESSION['kj_username'] = $email;
+            $_SESSION['kj_authorized'] = "1";   
+			header('Location: ' . filter_var($login_seccess_url  , FILTER_SANITIZE_URL));
+			
+		
+		}
+	}
 }
 
- ?> 
+ 
+mysql_free_result($rs_checkfbuser);
+?>
