990bcce26038ff7605eed391c375acf005a50a5b
nawissor <shutterman@iwanross.co.za>
2018-02-10 10:25:45 +0200

    attempting to login user after verifying email

--- admin/verifymail.php
+++ admin/verifymail.php
@@ -1,7 +1,52 @@
+<?php require_once('../Connections/killjoy.php'); ?>
 <?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$colname_rs_verifymail = "-1";
+if (isset($_GET['verifier'])) {
+  $colname_rs_verifymail = $_GET['verifier'];
+}
+mysql_select_db($database_killjoy, $killjoy);
+$query_rs_verifymail = sprintf("SELECT g_name, g_email, g_pass FROM social_users WHERE g_email = %s", GetSQLValueString($colname_rs_verifymail, "text"));
+$rs_verifymail = mysql_query($query_rs_verifymail, $killjoy) or die(mysql_error());
+$row_rs_verifymail = mysql_fetch_assoc($rs_verifymail);
+$totalRows_rs_verifymail = mysql_num_rows($rs_verifymail);
+$name = $row_rs_verifymail['g_name'];
+$hashedpassword = $row_rs_verifymail['g_pass'];
+echo $hashedpassword;
 
 
-if (isset($_POST['g_email'])) {
+
+if (isset($_GET['verifier'])) {
   $loginUsername=$_POST['g_email'];
   $loginPassword=$_POST['g_pass'];
   $MM_fldUserAuthorization = "";
@@ -48,3 +93,6 @@ if (isset($_POST['g_email'])) {
 <body>
 </body>
 </html>
+<?php
+mysql_free_result($rs_verifymail);
+?>
