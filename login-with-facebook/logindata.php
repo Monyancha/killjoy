39ac1185fbd3deab0d43b080487b3bf0bf3f84a8
nawissor <shutterman@iwanross.co.za>
2018-02-10 12:48:51 +0200

    checking if user exists for facebook login

--- login-with-facebook/logindata.php
+++ login-with-facebook/logindata.php
@@ -15,19 +15,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 		$active = "1";
 		$profilePictureUrl = $_POST['profilePictureUrl'];
 		$locale = $_POST['link'];
+		
+		$result = mysql_query("SELECT COUNT(g_email) FROM social_users WHERE g_email=$email");
+	if($result === false) { 
+		die(mysql_error()); //result is false show db error and exit.
+	}
 	
-		$query = "INSERT INTO social_users(g_name,g_email,g_id,g_image,g_link, g_active) VALUES ('".$name."','".$email."','".$fb_Id."','".$profilePictureUrl."','".$locale."','".$active."')";
-		$result = mysqli_query($conn , $query) or die(mysqli_error());
-		if ($result) {			
-	  
-			$_SESSION['kj_username'] = $email;
-            $_SESSION['kj_authorized'] = "1";   
-					}else{
-			echo "Oeps";
-		}
+	$UserCount = mysql_fetch_array($result);
+ 
+    if($UserCount[0]) //user id exist in database
+    {
+		echo 'Welcome back '.$user_name.'!';
+    }else{ //user is new
+		echo 'Hello! '.$user_name.', Thanks for Registering!';
+		@mysql_query("INSERT INTO social_users(g_name,g_email,g_id,g_image,g_link, g_active) VALUES ('".$name."','".$email."','".$fb_Id."','".$profilePictureUrl."','".$locale."','".$active."')");
+	}
 
-}else{
-	echo "Try again Later";
+	$_SESSION['kj_username'] = $email;
+    $_SESSION['kj_authorized'] = "1";  
+	header('Location: ' . filter_var($login_seccess_url  , FILTER_SANITIZE_URL));
+		
+		
+	
 }
 
  ?> 
