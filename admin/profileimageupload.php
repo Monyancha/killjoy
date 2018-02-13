73a7d7950021db55ac90a17f595676537cdee641
nawissor <shutterman@iwanross.co.za>
2018-02-13 15:11:14 +0200

    creating record sources for upload errors and messages

--- admin/profileimageupload.php
+++ admin/profileimageupload.php
@@ -1,3 +1,4 @@
+<?php require_once('../Connections/killjoy.php'); ?>
 <?php
 ob_start();
 if (!isset($_SESSION)) {
@@ -5,11 +6,44 @@ session_start();
 }
 require_once('Connections/killjoy.php');
 
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+
+
 
 if (isset($_SESSION['sessionid'])) {
 $sessionid = $_SESSION['sessionid'];
 }
-$path = "../admin/images/products/";
+$path = "images/members/";
 chdir ($path);
 if (!file_exists($sessionid) && !is_dir($sessionid)) {
 mkdir ($sessionid,0777,true);
@@ -17,7 +51,6 @@ mkdir ($sessionid,0777,true);
 $errors = array();
 $uploadedFiles = array();
 $extension = array("gif", "jpeg", "jpg", "png","GIF","JPEG","JPG","PNG");
-$anchor = "#photo";
 $totalBytes = 2097152;
 $UploadFolder = "$sessionid/";
 $dir = $UploadFolder;
@@ -54,11 +87,13 @@ move_uploaded_file($temp,$UploadFolder."/".$name);
 array_push($uploadedFiles, $name);						
 $successmsg = $counter." images successfully uploaded";
 
+
   $deleteSQL = sprintf("DELETE FROM tbl_uploaderror WHERE sessionid=%s",
                        GetSQLValueString($_SESSION['sessionid'], "text"));
 
-  mysqli_select_db( $stomer, $database_stomer);
-  $Result1 = mysqli_query( $stomer, $deleteSQL) or die(mysqli_error($GLOBALS["___mysqli_ston"]));
+  mysql_select_db($database_killjoy, $killjoy);
+  $Result1 = mysql_query($deleteSQL, $killjoy) or die(mysql_error());
+
 
 foreach($uploadedFiles as $fileName);
 
@@ -67,8 +102,8 @@ foreach($uploadedFiles as $fileName);
                        GetSQLValueString($sessionid, "text"),
                        GetSQLValueString($successmsg, "text"));
 
-  mysqli_select_db( $stomer, $database_stomer);
-  $Result1 = mysqli_query( $stomer, $insertSQL) or die(mysqli_error($GLOBALS["___mysqli_ston"]));
+  mysql_select_db($database_killjoy, $killjoy);
+  $Result1 = mysql_query($insertSQL, $killjoy) or die(mysql_error());
 }
 }
 if($counter>0){
@@ -78,8 +113,8 @@ if(count($errors)>0)
   $deleteSQL = sprintf("DELETE FROM tbl_uploaderror WHERE sessionid=%s",
                        GetSQLValueString($_SESSION['sessionid'], "text"));
 
-  mysqli_select_db( $stomer, $database_stomer);
-  $Result1 = mysqli_query( $stomer, $deleteSQL) or die(mysqli_error($GLOBALS["___mysqli_ston"]));
+  mysql_select_db($database_killjoy, $killjoy);
+  $Result1 = mysql_query($deleteSQL, $killjoy) or die(mysql_error());
   
 foreach($errors as $error)
 {
