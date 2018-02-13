9e17f3956b8f639833dac3b52fa86f0a60a4fb8f
nawissor <shutterman@iwanross.co.za>
2018-02-13 15:01:23 +0200

    creating image upload script

--- admin/profileimageupload.php
+++ admin/profileimageupload.php
@@ -3,49 +3,8 @@ ob_start();
 if (!isset($_SESSION)) {
 session_start();
 }
-require_once('../Connections/stomer.php');
+require_once('Connections/killjoy.php');
 
-if (!function_exists("GetSQLValueString")) {
-function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
-{
-  if (PHP_VERSION < 6) {
-    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
-  }
-
-  $theValue = function_exists("mysqli_real_escape_string") ? mysqli_real_escape_string($GLOBALS["___mysqli_ston"], $theValue) : ((isset($GLOBALS["___mysqli_ston"]) && is_object($GLOBALS["___mysqli_ston"])) ? mysqli_real_escape_string($GLOBALS["___mysqli_ston"], $theValue) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""));
-
-  switch ($theType) {
-    case "text":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;    
-    case "long":
-    case "int":
-      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
-      break;
-    case "double":
-      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
-      break;
-    case "date":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;
-    case "defined":
-      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
-      break;
-  }
-  return $theValue;
-}
-}
-
-$colname_select_user = "-1";
-if (isset($_SESSION['MM_Username'])) {
-  $colname_select_user = $_SESSION['MM_Username'];
-}
-mysqli_select_db( $stomer, $database_stomer);
-$query_select_user = sprintf("SELECT st_username, st_userid FROM st_users WHERE st_username = %s", GetSQLValueString($colname_select_user, "text"));
-$select_user = mysqli_query( $stomer, $query_select_user) or die(mysqli_error($GLOBALS["___mysqli_ston"]));
-$row_select_user = mysqli_fetch_assoc($select_user);
-$totalRows_select_user = mysqli_num_rows($select_user);
-$member = $row_select_user['st_userid'];
 
 if (isset($_SESSION['sessionid'])) {
 $sessionid = $_SESSION['sessionid'];
