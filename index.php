27e3e7efe87711a2bc3043921b17102ab1620327
nawissor <shutterman@iwanross.co.za>
2018-02-13 10:19:01 +0200

    creating member profile div

--- index.php
+++ index.php
@@ -135,6 +135,7 @@ $totalRows_rs_social_users = mysql_num_rows($rs_social_users);
     <?php if ($totalRows_rs_social_users > 0) { // Show if recordset not empty ?>
       <a href="#"><div class="profile" id="profile"><img id="profile_image" src="<?php echo $row_rs_social_users['g_image']; ?>" alt="killjoy - rental property reviews and advice"></div></a>
       <?php } // Show if recordset not empty ?>
+      <div class="memberprofile" id="memberprofile">Hallo there</div>
 <?php if($showsignin == 0) { ?><div class="signin" id="signin"><a href="admin/index.php"><font size="+2">Sign in</font></a></div><?php } ?>
     <?php if($showsignin == 1) { ?><div class="signout" id="signout"><a href="admin/logout.php"><font size="+2">Sign out</font></a></div><?php } ?>
   </div>  
