81af84d56046567b47ec9b7913fd89b5b72ae9ac
nawissor <shutterman@iwanross.co.za>
2018-02-15 15:07:48 +0200

    creating login if user is remembered

--- index.php
+++ index.php
@@ -5,6 +5,47 @@ ob_start();
 if (!isset($_SESSION)) {
 session_start();
 }
+
+$colname_rs_kj_recall = "-1";
+if (isset($_COOKIE['kj_recallmember'])) {
+  $colname_rs_kj_recall = $_COOKIE['kj_recallmember'];
+
+mysql_select_db($database_killjoy, $killjoy);
+$query_rs_kj_recall = sprintf("SELECT g_name, g_email, g_active FROM social_users WHERE g_email = %s AND g_active = 1", GetSQLValueString($colname_rs_kj_recall, "text"));
+$rs_kj_recall = mysql_query($query_rs_kj_recall, $killjoy) or die(mysql_error());
+$row_rs_kj_recall = mysql_fetch_assoc($rs_kj_recall);
+$totalRows_rs_kj_recall = mysql_num_rows($rs_kj_recall);
+
+
+
+  $loginUsername=$row_rs_verifymail['g_email'];
+  $loginPassword=$row_rs_verifymail['g_plain'];
+  $MM_fldUserAuthorization = "";
+  $MM_redirectLoginSuccess = "../index.php";
+  $MM_redirectLoginFailed = "registernew.php";
+  $MM_redirecttoReferrer = false;
+  mysql_select_db($database_killjoy, $killjoy);
+  
+  $LoginRS__query=sprintf("SELECT g_email, g_plain FROM social_users WHERE g_email=%s AND g_plain=%s",
+    GetSQLValueString($loginUsername, "text"), GetSQLValueString($loginPassword, "text")); 
+   
+  $LoginRS = mysql_query($LoginRS__query, $killjoy) or die(mysql_error());
+  $loginFoundUser = mysql_num_rows($LoginRS);
+  if ($loginFoundUser) {
+     $loginStrGroup = "";
+    
+	if (PHP_VERSION >= 5.1) {session_regenerate_id(true);} else {session_regenerate_id();}
+    //declare two session variables and assign them
+    $_SESSION['kj_username'] = $loginUsername;
+    $_SESSION['kj_usergroup'] = $loginStrGroup;	
+	$_SESSION['kj_authorized'] = "1";     
+
+    if (isset($_SESSION['PrevUrl']) && false) {
+      $MM_redirectLoginSuccess = $_SESSION['PrevUrl'];	
+    }
+  }
+}
+
 $showsignin = -1;
 if(isset($_SESSION['kj_authorized'])) {
 	
@@ -55,6 +96,7 @@ $query_rs_social_users = sprintf("SELECT g_name, g_email, g_image FROM social_us
 $rs_social_users = mysql_query($query_rs_social_users, $killjoy) or die(mysql_error());
 $row_rs_social_users = mysql_fetch_assoc($rs_social_users);
 $totalRows_rs_social_users = mysql_num_rows($rs_social_users);
+
 
 
 
@@ -216,4 +258,6 @@ $(document).ready(
 </html>
 <?php
 mysql_free_result($rs_social_users);
+
+mysql_free_result($rs_kj_recall);
 ?>
