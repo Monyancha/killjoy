4c8180f36e2124a4b3791e4bafb4d3de280bedca
nawissor <shutterman@iwanross.co.za>
2018-02-15 13:28:27 +0200

    remember me button added and styled

--- admin/index-signin.php
+++ admin/index-signin.php
@@ -94,7 +94,12 @@ if(isset($authUrl)) //user is not logged in, show login button
 	echo '<div class="usemail">Or Continue with your Email</div>';
 	echo '<input class="usermail" type="email" name="usermail" id="usermail" />';
 	echo '<br />';
+	echo '<input type="checkbox" name="remember_me" id="remember_me" value="1" />';
+	echo '<label for="remember_me">Remember Me</label>';
+	echo '<br />';
 	echo '<button class="nextbutton">Next <span class="icon-arrow-circle-right"></span></button>';
+	echo '<br />';
+	
 	echo '</form>';
 	echo '</div>';
 } 
@@ -135,3 +140,7 @@ echo '</body></html>';
 ?>
 <link href="../iconmoon/style.css" rel="stylesheet" type="text/css" />
 <link href="css/checks.css" rel="stylesheet" type="text/css" />
+
+
+
+
