6dc42d5d2c57a92535a737c9d37e0fb09e979bc1
nawissor <shutterman@iwanross.co.za>
2018-02-15 10:57:19 +0200

    pages created to change account email address

---
+++ admin/changemail.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="canonical" href="https://www.killjoy.co.za/index.php">
+<title>killjoy - registration page</title>
+<script type="text/javascript" src="../fancybox/lib/jquery-1.9.0.min.js"></script>
+<link rel="stylesheet" href="../fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
+<script type="text/javascript" src="../fancybox/source/jquery.fancybox.pack.js?v=2.1.5"></script>
+</head>
+
+<body>
+<div id="viewreviews"><?php include'changeemail.php';?></div>
+
+<script type="text/javascript">
+var $j = jQuery.noConflict();
+$j(document).ready(function() {
+ $j.fancybox({
+	 helpers : {
+overlay : {
+css : {
+  'background' : 'rgba(200, 201, 203, 0.40)'
+   }
+}
+},
+href: '#viewreviews', 
+modal: false,
+ 'afterClose'  : function() {			   
+ location.href ="../member.php";		
+		 
+ },
+ 
+ });
+return false;
+});
+ 
+</script>
+</body>
+</html>
