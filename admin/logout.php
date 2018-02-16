ee80e570389cf2f7d13926f153533e73c2cd50c3
nawissor <shutterman@iwanross.co.za>
2018-02-16 10:28:19 +0200

    it is the small things that bite!
    should have used if not isset!

--- admin/logout.php
+++ admin/logout.php
@@ -7,4 +7,14 @@ $_SESSION = array();
 unset($_SESSION);
 session_destroy();
 header("location:index.php");
+
+if (isset($_SERVER['HTTP_COOKIE'])) {
+    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+    foreach($cookies as $cookie) {
+        $parts = explode('=', $cookie);
+        $name = trim($parts[0]);
+        setcookie($name, '', time()-1000);
+        setcookie($name, '', time()-1000, '/');
+    }
+}
 ?>
