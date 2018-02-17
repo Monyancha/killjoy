1c44192dddde0961bd0f4f7e46d964498efa4c30
nawissor <shutterman@iwanross.co.za>
2018-02-17 11:30:25 +0200

    created code to generate a long sessionid

--- reviewer.php
+++ reviewer.php
@@ -30,6 +30,16 @@ function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDe
   return $theValue;
 }
 }
+function generateRandomString($length = 10) {
+$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+$charactersLength = strlen($characters);
+$randomString = '';
+for ($i = 0; $i < $length; $i++) {
+$randomString .= $characters[rand(0, $charactersLength - 1)];
+}
+return $randomString;
+}
+$sessionid = generateRandomString();
 
 
 ?>
