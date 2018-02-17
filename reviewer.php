4e6e17ff91afe63df350bcf0164014bc82da6b12
nawissor <shutterman@iwanross.co.za>
2018-02-17 13:21:56 +0200

    creating temp image for NULL images

--- reviewer.php
+++ reviewer.php
@@ -87,6 +87,14 @@ if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "addressField")) {
   mysql_select_db($database_killjoy, $killjoy);
   $Result1 = mysql_query($insertSQL, $killjoy) or die(mysql_error());
   
+  	$propertysession = $_POST['txt_szessionid'];
+	 $insertSQL = sprintf("INSERT INTO tbl_propertyimages (sessionid, image_url) VALUES (%s, %s)",
+                       GetSQLValueString($_POST['txt_szessionid'], "text"),
+                       GetSQLValueString($_POST['country'], "text"));
+
+  mysql_select_db($database_killjoy, $killjoy);
+  $Result1 = mysql_query($insertSQL, $killjoy) or die(mysql_error());
+  
 
 
   $insertGoTo = "reviewsteptwo.php";
