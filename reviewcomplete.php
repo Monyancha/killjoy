bb15da155a220d733d21dc0cd6e95a8b0dfa509c
nawissor <shutterman@iwanross.co.za>
2018-02-19 12:19:50 +0200

    review complete message completed

--- reviewcomplete.php
+++ reviewcomplete.php
@@ -1,8 +1,63 @@
+<?php require_once('Connections/killjoy.php'); ?>
 <?php
 ob_start();
 if (!isset($_SESSION)) {
 session_start();
 }
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$colname_rs_social_user = "-1";
+if (isset($_SESSION['kj_username'])) {
+  $colname_rs_social_user = $_SESSION['kj_username'];
+}
+mysql_select_db($database_killjoy, $killjoy);
+$query_rs_social_user = sprintf("SELECT g_name, g_email FROM social_users WHERE g_email = %s", GetSQLValueString($colname_rs_social_user, "text"));
+$rs_social_user = mysql_query($query_rs_social_user, $killjoy) or die(mysql_error());
+$row_rs_social_user = mysql_fetch_assoc($rs_social_user);
+$totalRows_rs_social_user = mysql_num_rows($rs_social_user);
+
+$colname_rs_showproperty = "-1";
+if (isset($_SESSION['kj_propsession'])) {
+  $colname_rs_showproperty = $_SESSION['kj_propsession'];
+}
+mysql_select_db($database_killjoy, $killjoy);
+$query_rs_showproperty = sprintf("SELECT * FROM tbl_address WHERE sessionid = %s", GetSQLValueString($colname_rs_showproperty, "text"));
+$rs_showproperty = mysql_query($query_rs_showproperty, $killjoy) or die(mysql_error());
+$row_rs_showproperty = mysql_fetch_assoc($rs_showproperty);
+$totalRows_rs_showproperty = mysql_num_rows($rs_showproperty);
+$streetnr = $row_rs_showproperty['str_number'];
+$street = $row_rs_showproperty['street_name'];
+$city = $row_rs_showproperty['city'];
+ 
 
 ?>
 
@@ -13,7 +68,7 @@ session_start();
 <META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="canonical" href="https://www.killjoy.co.za/index.php">
-<title>killjoy - registration page</title>
+<title>killjoy - rental property review completed</title>
 <script type="text/javascript" src="fancybox/lib/jquery-1.9.0.min.js"></script>
 <link rel="stylesheet" href="fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
 <script type="text/javascript" src="fancybox/source/jquery.fancybox.pack.js?v=2.1.5"></script>
@@ -21,7 +76,7 @@ session_start();
 </head>
 
 <body>
-<div id="notexist" class="completeexist"><div class="completecells">Thank you <?php echo $name ?></div><div class="completecells">A password reset link has been mailed to <?php echo $email ?></div><div class="completecells">Please follow the instructions in the email to reset your password</div><div class="completecells"><a href="index.php">Close</a></div></div>;
+<div id="notexist" class="completeexist"><div class="completecells">Thank you <?php echo $row_rs_social_user['g_name']; ?> form making South Africa a better place!</div><div class="completecells">Your review of <strong><?php echo $streetnr ?>&nbsp;<?php echo $street ?>&nbsp;<?php echo $city ?></strong> has been recorded.</div><div class="completecells">Please note that your review is under assessment from one of our editors and will be published as soon as the editor approves of the the content in your review. All reviews are subject to the Terms and Conditions as stipulated by our <a href='info-centre/fair-review-policy.html'>Fair Review Policy</a></div><div class="completecells"><a href="index.php">Close</a></div></div>;
 
 <script type="text/javascript">
 var $j = jQuery.noConflict();
