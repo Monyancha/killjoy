d8acf414f419f1c3871b215ceb74049a47a94b75
nawissor <shutterman@iwanross.co.za>
2018-02-22 16:53:54 +0200

    created seperate record source for rating

--- viewreviews.php
+++ viewreviews.php
@@ -42,10 +42,20 @@ if (isset($_GET['claw'])) {
   $colname_rs_show_review = $_GET['claw'];
 }
 mysql_select_db($database_killjoy, $killjoy);
-$query_rs_show_review = sprintf("SELECT tbl_address.sessionid as propsession, tbl_address.str_number as streetnumber, tbl_address.street_name as streetname, tbl_address.city as city, tbl_address.postal_code AS postalCode, DATE_FORMAT(tbl_address_comments.rating_date, '%%d-%%b-%%y')AS ratingDate, ROUND(AVG(tbl_address_rating.rating_value),2) AS Avgrating, COUNT(tbl_address_rating.id) AS ratingCount, IFNULL(tbl_propertyimages.image_url,'images/icons/house-outline-bg.png') AS propertyImage FROM tbl_address LEFT JOIN tbl_address_comments ON tbl_address_comments.sessionid = tbl_address.sessionid LEFT JOIN tbl_address_rating ON tbl_address_rating.sessionid = tbl_address.sessionid LEFT JOIN tbl_propertyimages ON tbl_propertyimages.sessionid = tbl_address.sessionid LEFT JOIN tbl_approved ON tbl_approved.sessionid = tbl_address.sessionid WHERE tbl_address.sessionid = %s GROUP BY tbl_address.sessionid ORDER BY tbl_address_comments.rating_date DESC", GetSQLValueString($colname_rs_show_review, "text"));
+$query_rs_show_review = sprintf("SELECT DISTINCT tbl_address.sessionid as propsession, tbl_address.str_number as streetnumber, tbl_address.street_name as streetname, tbl_address.city as city, tbl_address.postal_code AS postalCode, DATE_FORMAT(tbl_address_comments.rating_date, '%%d-%%b-%%y')AS ratingDate, IFNULL(tbl_propertyimages.image_url,'images/icons/house-outline-bg.png') AS propertyImage FROM tbl_address LEFT JOIN tbl_address_comments ON tbl_address_comments.sessionid = tbl_address.sessionid LEFT JOIN tbl_address_rating ON tbl_address_rating.sessionid = tbl_address.sessionid LEFT JOIN tbl_propertyimages ON tbl_propertyimages.sessionid = tbl_address.sessionid LEFT JOIN tbl_approved ON tbl_approved.sessionid = tbl_address.sessionid WHERE tbl_address.sessionid = %s GROUP BY tbl_address.sessionid ORDER BY tbl_address_comments.rating_date DESC", GetSQLValueString($colname_rs_show_review, "text"));
 $rs_show_review = mysql_query($query_rs_show_review, $killjoy) or die(mysql_error());
 $row_rs_show_review = mysql_fetch_assoc($rs_show_review);
 $totalRows_rs_show_review = mysql_num_rows($rs_show_review);
+
+$colname_rs_show_rating = "-1";
+if (isset($_GET['claw'])) {
+  $colname_rs_show_rating = $_GET['claw'];
+}
+mysql_select_db($database_killjoy, $killjoy);
+$query_rs_show_rating = sprintf("SELECT ROUND(AVG(tbl_address_rating.rating_value),2) AS Avgrating, COUNT(tbl_address_rating.id) AS ratingCount FROM tbl_address_rating WHERE sessionid = %s", GetSQLValueString($colname_rs_show_rating, "text"));
+$rs_show_rating = mysql_query($query_rs_show_rating, $killjoy) or die(mysql_error());
+$row_rs_show_rating = mysql_fetch_assoc($rs_show_rating);
+$totalRows_rs_show_rating = mysql_num_rows($rs_show_rating);
 ?>
 
 
@@ -60,6 +70,44 @@ $totalRows_rs_show_review = mysql_num_rows($rs_show_review);
 <title>Killjoy - see the review for <?php echo $row_rs_show_review['streetnumber']; ?>, <?php echo $row_rs_show_review['streetname']; ?>, <?php echo $row_rs_show_review['city']; ?>, <?php echo $row_rs_show_review['postalCode']; ?></title>
 <link href="css/view-reviews/profile.css" rel="stylesheet" type="text/css" />
 <link href="iconmoon/style.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript">
+   	$.fn.stars = function() {
+    return $(this).each(function() {
+        // Get the value
+        var val = parseFloat($(this).html());
+        // Make sure that the value is in 0 - 5 range, multiply to get width
+        var size = Math.max(0, (Math.min(5, val))) * 32;
+        // Create stars holder
+        var $span = $('<span />').width(size);
+        // Replace the numerical value with stars
+        $(this).html($span);
+    });
+}
+	</script>
+
+	<style type="text/css">
+	span.stars, span.stars span {
+	display: inline-block;
+	height: 32px;
+	background-image: url(images/stars/property-rating.png);
+	background-repeat: repeat-x;
+	background-position: 0 -32px;
+	vertical-align: middle;
+	width: 160px;
+}
+
+span.stars span {
+    background-position: 0 0;
+}
+	.videoholder {
+	margin-right: auto;
+	margin-left: auto;
+	padding: 5px;
+	height: 280px;
+	width: 400px;
+}
+    </style>
+
 </head>
 <body onLoad="set_session()">
 
@@ -67,23 +115,27 @@ $totalRows_rs_show_review = mysql_num_rows($rs_show_review);
   <div class="formheader">Killjoy.co.za Property Review</div>
 <div class="imagebox" id="imagebox">  
 <img src="<?php echo $row_rs_show_review['propertyImage']; ?>" alt="killjoy.co.za member profile image" class="propertyimage" /> 
-<div class="addressfield"><address><?php echo $row_rs_show_review['streetnumber']; ?> <?php echo $row_rs_show_review['streetname']; ?><br /><?php echo $row_rs_show_review['city']; ?><br /><?php echo $row_rs_show_review['postalCode']; ?></address></div>
-         
+<div class="addressfield"><address><?php echo $row_rs_show_review['streetnumber']; ?> <?php echo $row_rs_show_review['streetname']; ?><br /><?php echo $row_rs_show_review['city']; ?><br /><?php echo $row_rs_show_review['postalCode']; ?></address></div>    
   </div>
-<div class="logoloaderrors" id="logoloaderror"><ol></li>
-</ol>
-</div>
-  <div class="fieldlabels" id="fieldlabels">Your name:<?php echo $row_rs_show_review['streetname']; ?></div>
-  <div class="formfields" id="formfields"><span id="sprytextfield1"><span class="textfieldRequiredMsg">!</span></span></div>
+
+  <div class="fieldlabels" id="fieldlabels">Rating:</div>
+  <div class="ratingbox"><span class="stars" id="stars"><?php echo $row_rs_show_rating['Avgrating']; ?></span> <?php echo $row_rs_show_rating['Avgrating']; ?> From: <?php echo $row_rs_show_rating['ratingCount']; ?></div>
     <div class="fieldlabels" id="fieldlabels">Your email:</div>
-    <div class="fieldlabels" id="fieldlabels">Date Joined:</div>
+    <div class="fieldlabels" id="fieldlabels">Review Date:</div>
 </div>
 
 
-
-
+</script>
+<script type="text/javascript">
+$(function() {
+$('span.stars').stars();
+});
+  </script>
+    
 </body>
 </html>
 <?php
 mysql_free_result($rs_show_review);
+
+mysql_free_result($rs_show_rating);
 ?>
