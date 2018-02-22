e32f6fb6f94a05c1c61415d0f46eed2fe06bafe7
nawissor <shutterman@iwanross.co.za>
2018-02-22 14:37:22 +0200

    view reviews pages created

---
+++ viewer.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="canonical" href="https://www.killjoy.co.za/index.php">
+<title>killjoy - member profile page</title>
+<script type="text/javascript" src="fancybox/lib/jquery-1.9.0.min.js"></script>
+<link rel="stylesheet" href="fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
+<script type="text/javascript" src="fancybox/source/jquery.fancybox.pack.js?v=2.1.5"></script>
+</head>
+
+<body>
+<div id="viewreviews"><?php include'viewreviews.php';?></div>
+
+<script type="text/javascript">
+$(document).ready(function() {
+ $.fancybox({
+	 helpers : {
+overlay : {
+css : {
+  'background' : 'rgba(200, 201, 203, 0.40)'
+   }
+}
+},
+href: '#viewreviews', 
+modal: false,
+ 'afterClose'  : function() {			   
+ location.href ="index.php";		
+		 
+ },
+ 
+ });
+return false;
+});
+ 
+</script>
+</body>
+</html>
