005152c799c970fa4b9adb48e247eddd361f5684
nawissor <shutterman@iwanross.co.za>
2018-02-25 10:28:00 +0200

    pages created for social click scripts

---
+++ functions/facebook_clicks.php
@@ -0,0 +1,71 @@
+<?php require_once('file:///C|/xampp/htdocs/stomer/Connections/stomer.php'); ?>
+<?php
+ob_start();
+if (!isset($_SESSION)) {
+session_start();
+}
+require_once('file:///C|/xampp/htdocs/stomer/Connections/stomer.php');
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysqli_real_escape_string") ? mysqli_real_escape_string($GLOBALS["___mysqli_ston"], $theValue) : ((isset($GLOBALS["___mysqli_ston"]) && is_object($GLOBALS["___mysqli_ston"])) ? mysqli_real_escape_string($GLOBALS["___mysqli_ston"], $theValue) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""));
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+
+
+if (isset($_POST["recipe_id"]));  {  
+$rowID = $_POST["recipe_id"]; // ◄■■ PARAMETER FROM AJAX.
+$_SESSION['recipe_id'] = $rowID;
+
+$insertSQL = sprintf("INSERT INTO social_clicks (recipe_id, facebook_clicks) VALUES (%s, %s)",
+                       GetSQLValueString($rowID, "int"),
+                       GetSQLValueString("1", "int"));
+					   
+					     mysqli_select_db( $stomer, $database_stomer);
+  $Result1 = mysqli_query( $stomer, $insertSQL) or die(mysqli_error($GLOBALS["___mysqli_ston"]));
+					  
+
+
+}
+ ?>
+ <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="content-language" content="en-za">
+<link rel="canonical" href="https://www.stomer.co.za/products.php">
+<title>Review a rental property - rate a rental property - rental property ratings</title>
+<meta name="description" content="Rent-a-Guuide a place where tenants can share their personal experience of living at a rental property. Rate and review a rental property" />
+<meta name="keywords" content="rental, property, review, rate, complaints, share, " />
+</head>
+ <body>
+ <?php include_once("file:///C|/xampp/htdocs/stomer/functions/analyticstracking.php") ?>
+</body>
+</html>
+
