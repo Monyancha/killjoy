90220c9a52a705e61baf976c5ec1826359b268be
nawissor <shutterman@iwanross.co.za>
2018-03-01 11:13:03 +0200

    social comments div created and styled

--- viewreviews.php
+++ viewreviews.php
@@ -186,8 +186,9 @@ span.stars span {
   <div class="fieldlabels" id="fieldlabels3">The shared experience:</div>
  <div class="commentbox"><?php echo $row_rs_show_review['comments']; ?></div>
  <div class="socialicons" id="socialicons"><div class="fb-like" data-layout="button_count" data-action="like" data-href="<?php echo $page ?>" data-size="large" data-show-faces="false" data-share="true"></div><div class="gplus-share"><div class="g-plus" data-action="share" data-height="42" data-href="<?php echo $page ?>"></div><div class="in-share"><script type="IN/Share" data-url="<?php echo json_encode($page) ?>" ></script></div></div>
-
 </div>
+<div class="social_comments"></div>
+
 </div>
 
 <script src="//platform.linkedin.com/in.js" type="text/javascript"> lang: en_US</script>
