010fbbb140dddaac8b407e23fc67687ef1723323
nawissor <shutterman@iwanross.co.za>
2018-03-04 09:47:46 +0200

    dynamic links added to individual reviews

--- memberreviews.php
+++ memberreviews.php
@@ -126,18 +126,42 @@ $totalRows_rs_my_reviews = mysql_num_rows($rs_my_reviews);
 <title>Killjoy - view or change your killjoy.co.za member reviews</title>
 <link href="css/member-reviews/profile.css" rel="stylesheet" type="text/css" />
 <link href="iconmoon/style.css" rel="stylesheet" type="text/css" />
+<link href="css/tooltips.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
 
 <div class="formcontainer" id="formcontainer">
 <div class="formheader">Killjoy.co.za Member Reviews</div>
-<?php do { ?>
-  <div class="reviewlist"><div class="imagebox"><img src="<?php echo $row_rs_my_reviews['propertyImage']; ?>" alt="property review image" class="propertyimage" /></div><div class="addressfield"><?php echo $row_rs_my_reviews['streetnumber']; ?> <?php echo $row_rs_my_reviews['streetname']; ?> <?php echo $row_rs_my_reviews['city']; ?></div></div>
+<?php do { $sessionid = filter_var($row_rs_my_reviews['propsession'], FILTER_SANITIZE_SPECIAL_CHARS); ?>
+   <a class="masterTooltip" title="view or edit your personal review for <?php echo $row_rs_my_reviews['streetnumber']; ?> <?php echo $row_rs_my_reviews['streetname']; ?> <?php echo $row_rs_my_reviews['city']; ?>" href="viewer.php?tarsus=<?php echo $captcha?>&claw=<?php echo $sessionid ?>&alula=<?php echo $smith ?>"><div class="reviewlist"><div class="imagebox"><img src="<?php echo $row_rs_my_reviews['propertyImage']; ?>" alt="property review image" class="propertyimage" /></div><div class="addressfield"><?php echo $row_rs_my_reviews['streetnumber']; ?> <?php echo $row_rs_my_reviews['streetname']; ?> <?php echo $row_rs_my_reviews['city']; ?></div></div>
   <?php } while ($row_rs_my_reviews = mysql_fetch_assoc($rs_my_reviews)); ?>
 <div class="accpetfield" id="accpetfield"> <div class="accepttext">Click on any of your reviews to make changes to the review </div></div>
 </div>
+</a>
 
-</form>
+<script type="text/javascript">
+$(document).ready(function() {
+// Tooltip only Text
+$('.masterTooltip').hover(function(){
+        // Hover over code
+        var title = $(this).attr('title');
+        $(this).data('tipText', title).removeAttr('title');
+        $('<p class="tooltip"></p>')
+        .text(title)
+        .appendTo('body')
+        .fadeIn('slow');
+}, function() {
+        // Hover out code
+        $(this).attr('title', $(this).data('tipText'));
+        $('.tooltip').remove();
+}).mousemove(function(e) {
+        var mousex = e.pageX + 20; //Get X coordinates
+        var mousey = e.pageY + 10; //Get Y coordinates
+        $('.tooltip')
+        .css({ top: mousey, left: mousex })
+});
+});
+</script>
 </body>
 </html>
 <?php
