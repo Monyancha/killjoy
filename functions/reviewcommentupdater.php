c2f20c9adbe4d4fb3007ee139d74a3ad48d8f49a
nawissor <shutterman@iwanross.co.za>
2018-03-05 09:31:20 +0200

    mail not sending?

--- functions/reviewcommentupdater.php
+++ functions/reviewcommentupdater.php
@@ -1,5 +1,9 @@
 <?php require_once('../Connections/killjoy.php'); ?>
 <?php
+ob_start();
+if (!isset($_SESSION)) {
+session_start();
+}
 if (!function_exists("GetSQLValueString")) {
 function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
 {
@@ -31,6 +35,29 @@ function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDe
 }
 }
 
+$colname_get_address = "-1";
+if (isset($_SESSION['sessionid'])) {
+  $colname_get_address = $_SESSION['sessionid'];
+}
+mysql_select_db($database_killjoy, $killjoy);
+$query_get_address = sprintf("SELECT * FROM tbl_address WHERE sessionid = %s", GetSQLValueString($colname_get_address, "text"));
+$get_address = mysql_query($query_get_address, $killjoy) or die(mysql_error());
+$row_get_address = mysql_fetch_assoc($get_address);
+$totalRows_get_address = mysql_num_rows($get_address);
+$streetnr = $row_get_address['str_number'];
+$street = $row_get_address['street_name'];
+$city = $row_get_address['city'];
+
+$colname_rs_social_user = "-1";
+if (isset($_SESSION['kj_username'])) {
+  $colname_rs_social_user = $_SESSION['kj_username'];
+}
+mysql_select_db($database_killjoy, $killjoy);
+$query_rs_social_user = sprintf("SELECT g_name, g_email FROM social_users WHERE g_email = %s", GetSQLValueString($colname_rs_social_user, "text"));
+$rs_social_user = mysql_query($query_rs_social_user, $killjoy) or die(mysql_error());
+$row_rs_social_user = mysql_fetch_assoc($rs_social_user);
+$totalRows_rs_social_user = mysql_num_rows($rs_social_user);
+
 if ((isset($_POST["txt_experience"])) && ($_POST["txt_experience"] != "")) {
   $updateSQL = sprintf("UPDATE tbl_address_comments SET rating_comments=%s WHERE sessionid=%s",
                        GetSQLValueString($_POST['txt_experience'], "text"),
@@ -46,14 +73,16 @@ if ((isset($_POST["txt_experience"])) && ($_POST["txt_experience"] != "")) {
   mysql_select_db($database_killjoy, $killjoy);
   $Result1 = mysql_query($updateSQL, $killjoy) or die(mysql_error());
   
-  date_default_timezone_set('Africa/Johannesburg');
+  
+  
+date_default_timezone_set('Africa/Johannesburg');
 $date = date('d-m-Y H:i:s');
 $time = new DateTime($date);
 $date = $time->format('d-m-Y');
 $time = $time->format('H:i:s');
 
-require('phpmailer-master/class.phpmailer.php');
-include('phpmailer-master/class.smtp.php');
+require('../phpmailer-master/class.phpmailer.php');
+include('../phpmailer-master/class.smtp.php');
 $name = $row_rs_social_user['g_name'];
 $email = $row_rs_social_user['g_email'];
 $email_1 = "friends@killjoy.co.za";
@@ -88,8 +117,8 @@ body {
 background-repeat: no-repeat;
 margin-left:50px;
 }
-</style></head><body>Dear ". $name ."<br><br>Thank you for making South Africa a better place!<br><br>Your review of <strong>".$row_rs_showproperty['str_number']."&nbsp;".$row_rs_showproperty['street_name']."&nbsp;".$row_rs_showproperty['city']."</strong> has been recorded and your reference number is: &nbsp;<strong><font color='#0000FF'><strong>".$_SESSION['kj_propsession']."</strong></font></strong><br><br>Please note that your review is under assessment from one of our editors and will be published as soon as the editor approves of the the content in your review. All reviews are subjected to the Terms and Conditions as stipulated by our <a href='info-centre/fair-review-policy.html'>Fair Review Policy</a>.<br><br>The rental property review was submitted by: <a href='mailto:$email'>$email</a> on $date at $time<br><br>If this was not you, please let us know by sending an email to: <a href='mailto:friends@killjoy.co.za'>Killjoy</a><br><br><br><br>Thank you, the Killjoy Community: https://www.killjoy.co.za<br><br><font size='2'>If you received this email by mistake, pleace let us know: <a href='mailto:friends@killjoy.co.za'>Killjoy</a></font><br><br></body></html>";
-$mail->Subject    = "Killjoy Review Completed";
+</style></head><body>Dear ". $name ."<br><br>You have made changes to a property review.</strong> <br><br>Your changes to the review of <strong>".$row_get_address['str_number']."&nbsp;".$row_get_address['street_name']."&nbsp;".$row_get_address['city']."</strong> has been recorded and your reference number is: &nbsp;<strong><font color='#0000FF'><strong>".$_SESSION['kj_propsession']."</strong></font></strong><br><br>Please note that your review is under assessment from one of our editors and will be published as soon as the editor approves of the the content in your review. All reviews are subjected to the Terms and Conditions as stipulated by our <a href='info-centre/fair-review-policy.html'>Fair Review Policy</a>.<br><br>The rental property review was submitted by: <a href='mailto:$email'>$email</a> on $date at $time<br><br>If this was not you, please let us know by sending an email to: <a href='mailto:friends@killjoy.co.za'>Killjoy</a><br><br><br><br>Thank you, the Killjoy Community: https://www.killjoy.co.za<br><br><font size='2'>If you received this email by mistake, pleace let us know: <a href='mailto:friends@killjoy.co.za'>Killjoy</a></font><br><br></body></html>";
+$mail->Subject    = "Killjoy Review Updated";
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 $body = "$message\r\n";
@@ -107,15 +136,7 @@ echo "Mailer Error: " . $mail->ErrorInfo;
 
 
 
-$colname_get_address = "-1";
-if (isset($_SESSION['sessionid'])) {
-  $colname_get_address = $_SESSION['sessionid'];
-}
-mysql_select_db($database_killjoy, $killjoy);
-$query_get_address = sprintf("SELECT * FROM tbl_address WHERE sessionid = %s", GetSQLValueString($colname_get_address, "text"));
-$get_address = mysql_query($query_get_address, $killjoy) or die(mysql_error());
-$row_get_address = mysql_fetch_assoc($get_address);
-$totalRows_get_address = mysql_num_rows($get_address);
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -125,11 +146,12 @@ $totalRows_get_address = mysql_num_rows($get_address);
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>killjoy - update member property experience field</title>
 </head>
-
 <body>
 
 </body>
 </html>
 <?php
 mysql_free_result($get_address);
+
+mysql_free_result($rs_social_user);
 ?>
