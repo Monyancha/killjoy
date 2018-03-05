1e238b9d3b6e43ad33d6499be90a986d9c1da7fd
nawissor <shutterman@iwanross.co.za>
2018-03-05 11:55:22 +0200

    yay! mail sending to members and admins on image change

--- functions/reviewimageupload.php
+++ functions/reviewimageupload.php
@@ -36,6 +36,16 @@ function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDe
 }
 }
 
+$colname_rs_social_user = "-1";
+if (isset($_SESSION['kj_username'])) {
+  $colname_rs_social_user = $_SESSION['kj_username'];
+}
+mysql_select_db($database_killjoy, $killjoy);
+$query_rs_social_user = sprintf("SELECT g_name, g_email FROM social_users WHERE g_email = %s", GetSQLValueString($colname_rs_social_user, "text"));
+$rs_social_user = mysql_query($query_rs_social_user, $killjoy) or die(mysql_error());
+$row_rs_social_user = mysql_fetch_assoc($rs_social_user);
+$totalRows_rs_social_user = mysql_num_rows($rs_social_user);
+
 $colname_get_address = "-1";
 if (isset($_SESSION['sessionid'])) {
   $colname_get_address = $_SESSION['sessionid'];
@@ -46,16 +56,46 @@ $get_address = mysql_query($query_get_address, $killjoy) or die(mysql_error());
 $row_get_address = mysql_fetch_assoc($get_address);
 $totalRows_get_address = mysql_num_rows($get_address);
 
-
-$colname_rs_social_user = "-1";
-if (isset($_SESSION['kj_username'])) {
-  $colname_rs_social_user = $_SESSION['kj_username'];
+require('../phpmailer-master/class.phpmailer.php');
+include('../phpmailer-master/class.smtp.php');
+$name = $row_rs_social_user['g_name'];
+$email = $row_rs_social_user['g_email'];
+$email_1 = "friends@killjoy.co.za";
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->Host = "killjoy.co.za";
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = "ssl";
+$mail->Username = "friends@killjoy.co.za";
+$mail->Password = "806Ppe##44VX";
+$mail->Port = "465";
+$mail->SetFrom('friends@killjoy.co.za', 'Killjoy Community');
+$mail->AddReplyTo("friends@killjoy.co.za","Killjoy Community");
+$message = "<html><head><style type='text/css'>
+a:link {
+text-decoration: none;
 }
-mysql_select_db($database_killjoy, $killjoy);
-$query_rs_social_user = sprintf("SELECT g_name, g_email FROM social_users WHERE g_email = %s", GetSQLValueString($colname_rs_social_user, "text"));
-$rs_social_user = mysql_query($query_rs_social_user, $killjoy) or die(mysql_error());
-$row_rs_social_user = mysql_fetch_assoc($rs_social_user);
-$totalRows_rs_social_user = mysql_num_rows($rs_social_user);
+a:visited {
+text-decoration: none;
+}
+a:hover {
+text-decoration: none;
+}
+a:active {
+text-decoration: none;
+}
+body,td,th {
+font-family: Tahoma, Geneva, sans-serif;
+font-size: 14px;
+}
+body {
+background-repeat: no-repeat;
+margin-left:50px;
+}
+</style></head><body>Dear ". $name ."<br><br>You have made changes to a property review.</strong> <br><br>Your changes to the review of <strong>".$row_get_address['str_number']."&nbsp;".$row_get_address['street_name']."&nbsp;".$row_get_address['city']."</strong> has been recorded and your reference number is: &nbsp;<strong><font color='#0000FF'><strong>".$_SESSION['sessionid']."</strong></font></strong><br><br>Please note that your review is under assessment from one of our editors and will be published as soon as the editor approves of the the content in your review. All reviews are subjected to the Terms and Conditions as stipulated by our <a href='info-centre/fair-review-policy.html'>Fair Review Policy</a>.<br><br>The rental property review was submitted by: <a href='mailto:$email'>$email</a> on $date at $time<br><br>If this was not you, please let us know by sending an email to: <a href='mailto:friends@killjoy.co.za'>Killjoy</a><br><br><br><br>Thank you, the Killjoy Community: https://www.killjoy.co.za<br><br><font size='2'>If you received this email by mistake, pleace let us know: <a href='mailto:friends@killjoy.co.za'>Killjoy</a></font><br><br></body></html>";
+
+
+
 
 
 
@@ -187,49 +227,12 @@ $updateSQL = sprintf("UPDATE tbl_approved SET was_checked=%s, checked_by=%s, is_
  }
  
  if ($Result1) {
-	date_default_timezone_set('Africa/Johannesburg');
+date_default_timezone_set('Africa/Johannesburg');
 $date = date('d-m-Y H:i:s');
 $time = new DateTime($date);
 $date = $time->format('d-m-Y');
 $time = $time->format('H:i:s');
 
-require('../phpmailer-master/class.phpmailer.php');
-include('../phpmailer-master/class.smtp.php');
-$name = $row_rs_social_user['g_name'];
-$email = $row_rs_social_user['g_email'];
-$email_1 = "friends@killjoy.co.za";
-$mail = new PHPMailer();
-$mail->IsSMTP();
-$mail->Host = "killjoy.co.za";
-$mail->SMTPAuth = true;
-$mail->SMTPSecure = "ssl";
-$mail->Username = "friends@killjoy.co.za";
-$mail->Password = "806Ppe##44VX";
-$mail->Port = "465";
-$mail->SetFrom('friends@killjoy.co.za', 'Killjoy Community');
-$mail->AddReplyTo("friends@killjoy.co.za","Killjoy Community");
-$message = "<html><head><style type='text/css'>
-a:link {
-text-decoration: none;
-}
-a:visited {
-text-decoration: none;
-}
-a:hover {
-text-decoration: none;
-}
-a:active {
-text-decoration: none;
-}
-body,td,th {
-font-family: Tahoma, Geneva, sans-serif;
-font-size: 14px;
-}
-body {
-background-repeat: no-repeat;
-margin-left:50px;
-}
-</style></head><body>Dear ". $name ."<br><br>You have made changes to a property review.</strong> <br><br>Your changes to the review of <strong>".$row_get_address['str_number']."&nbsp;".$row_get_address['street_name']."&nbsp;".$row_get_address['city']."</strong> has been recorded and your reference number is: &nbsp;<strong><font color='#0000FF'><strong>".$_SESSION['sessionid']."</strong></font></strong><br><br>Please note that your review is under assessment from one of our editors and will be published as soon as the editor approves of the the content in your review. All reviews are subjected to the Terms and Conditions as stipulated by our <a href='info-centre/fair-review-policy.html'>Fair Review Policy</a>.<br><br>The rental property review was submitted by: <a href='mailto:$email'>$email</a> on $date at $time<br><br>If this was not you, please let us know by sending an email to: <a href='mailto:friends@killjoy.co.za'>Killjoy</a><br><br><br><br>Thank you, the Killjoy Community: https://www.killjoy.co.za<br><br><font size='2'>If you received this email by mistake, pleace let us know: <a href='mailto:friends@killjoy.co.za'>Killjoy</a></font><br><br></body></html>";
 $mail->Subject    = "Killjoy Review Updated";
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
