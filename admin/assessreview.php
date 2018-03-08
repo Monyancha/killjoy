4a300242e54612dd9bbcd1702df358a753eeb51b
nawissor <shutterman@iwanross.co.za>
2018-03-08 12:31:59 +0200

    some message has the same id. Weird. Trying to resolve

--- admin/assessreview.php
+++ admin/assessreview.php
@@ -154,6 +154,9 @@ if(!$mail->Send()) {
 echo "Mailer Error: " . $mail->ErrorInfo;
 }
 
+$newsubject = $mail->Subject;
+$comments = $mail->msgHTML($body);
+
   $updateSQL = sprintf("UPDATE tbl_approved SET was_checked=%s, checked_by=%s, is_approved=%s WHERE sessionid=%s",
                        GetSQLValueString(1, "int"),
 					   GetSQLValueString($_SESSION['kj_adminUsername'], "text"),
@@ -162,6 +165,16 @@ echo "Mailer Error: " . $mail->ErrorInfo;
 
   mysql_select_db($database_killjoy, $killjoy);
   $Result1 = mysql_query($updateSQL, $killjoy) or die(mysql_error());
+  
+
+  $insertSQL = sprintf("INSERT INTO user_messages (u_email, sessionid, u_sunject, u_message) VALUES (%s, %s, %s, %s)",
+                       GetSQLValueString($email, "text"),
+					   GetSQLValueString($row_rs_show_comments['sessionid'], "text"),
+					   GetSQLValueString($newsubject , "text"),
+                       GetSQLValueString($comments, "text"));
+
+  mysql_select_db($database_killjoy, $killjoy);
+  $Result1 = mysql_query($insertSQL, $killjoy) or die(mysql_error());
 
 header('Location: ' . filter_var($register_seccess_url  , FILTER_SANITIZE_URL));
 
--- index.php
+++ index.php
@@ -159,7 +159,7 @@ if (isset($_SESSION['kj_username'])) {
   $colname_rs_member_message = $_SESSION['kj_username'];
 }
 mysql_select_db($database_killjoy, $killjoy);
-$query_rs_member_message = sprintf("SELECT * FROM user_messages WHERE u_email = %s AND u_read = %s", GetSQLValueString($colname_rs_user_message, "text"),GetSQLValueString(0, "int"));
+$query_rs_member_message = sprintf("SELECT * FROM user_messages WHERE u_email = %s AND u_read = %s ORDER BY u_date DESC", GetSQLValueString($colname_rs_user_message, "text"),GetSQLValueString(0, "int"));
 $rs_member_message = mysql_query($query_rs_member_message, $killjoy) or die(mysql_error());
 $row_rs_member_message = mysql_fetch_assoc($rs_member_message);
 $totalRows_rs_member_message = mysql_num_rows($rs_member_message);
