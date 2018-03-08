3b4e0b22553a85ad3c1d9f1305b5264a8e46f282
nawissor <shutterman@iwanross.co.za>
2018-03-08 11:56:46 +0200

    mark message as read when page opens

--- membermessages.php
+++ membermessages.php
@@ -78,6 +78,15 @@ function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDe
 }
 }
 
+
+  $updateSQL = sprintf("UPDATE user_messages SET u_read=%s WHERE sessionid=%s",
+                       GetSQLValueString(1, "int"),
+                       GetSQLValueString($_GET['claw'], "text"));
+
+  mysql_select_db($database_killjoy, $killjoy);
+  $Result1 = mysql_query($updateSQL, $killjoy) or die(mysql_error());
+
+
 $colname_rs_view_message = "-1";
 if (isset($_GET['claw'])) {
   $colname_rs_view_message = $_GET['claw'];
@@ -100,7 +109,6 @@ $totalRows_rs_view_message = mysql_num_rows($rs_view_message);
 <link href="iconmoon/style.css" rel="stylesheet" type="text/css" />
 </head>
 <body onLoad="set_session()">
-<form id="register" class="form" name="register" method="POST" action="myprofile.php">
 <div class="formcontainer" id="formcontainer">
 <div class="formheader">Killjoy.co.za Member Messages</div>
 <div class="fieldlabels">Subject:</div>
@@ -108,148 +116,11 @@ $totalRows_rs_view_message = mysql_num_rows($rs_view_message);
 <div class="fieldlabels">Message:</div>
 <div class="messagebox"><p><?php echo utf8_encode($row_rs_view_message['u_message']); ?></p></div>
 <div class="closefield"><a href="index.php">Close</a></div>
-
 </div>
-<input type="hidden" name="MM_insert" value="update" />
-</form>
-<script type="text/javascript">
- function acceptimage() {
-var data = new FormData();
-jQuery.each(jQuery('#files')[0].files, function(i, file) {
-data.append('file-'+i, file);
-data.append('txt_sesseyed', $("#txt_sesseyed").val());
- }); 
-$.ajax({
-url: 'admin/profileimageupload.php',
-data: data, 	
-enctype: 'multipart/form-data', 
-cache: false,
-contentType: false,
-processData: false,
-type: 'POST',
- beforeSend: function(){
-$('.uploader').show();
-},
-complete: function(){
-$('.uploader').hide(); // Handle the complete event
-},
-success : function (data)
-{ 
-  $('#logoloaderror').load(document.URL +  ' #logoloaderror');  
-    $('#imagebox').load(document.URL +  ' #imagebox');
-  
-  
-			  
-			
-},
-error   : function ( xhr )
-{ alert( "error" );
-}
- } );
-return false();	
-}
-</script>
-
-<script type="text/javascript">
- function set_session ( txt_sesseyed ) 
-{ $.ajax( { type    : "POST",
-data    : { "txt_sesseyed" : $("#txt_sesseyed").val()}, 
-url     : "admin/member_session.php",
-success : function (data)
-{ 
-  
-},
-error   : function ( xhr )
-{ alert( "error" );
-}
- } );
- return false;
- }
-</script>
-
-<script type="text/javascript">
-function unlink_thumb ( image_id ) 
-{ $.ajax( { type    : "POST",
-async   : false,
-data    : { "image_id" : image_id }, 
-url     : "admin/removeprofileimage.php",
-success : function ( image_id )
-{  $('#logoloaderror').load(document.URL +  ' #logoloaderror');  
-    $('#imagebox').load(document.URL +  ' #imagebox');
-						   
-},
-error   : function ( xhr )
-{ alert( "error" );
-}
- } );
- return false;
- }
-</script>
-
-<script type="text/javascript">
- function member_location ( location ) 
-{ $.ajax( { type    : "POST",
-data: $("input[name=location]:checked").serialize(),
-url     : "functions/userlocationupdater.php",
-success : function (location)
-		  {     
-		  $('#privacy').removeClass('formfields'); 
-		  $('#privacy').load(document.URL +  ' #privacy');  
-		   },
-		error   : function ( xhr )
-		  { alert( "error" );
-		  }
-		  
-} );
- return false;	
-}
-</script>
-
-<script type="text/javascript">
- function member_privacy ( anonymous ) 
-{ $.ajax( { type    : "POST",
-data: $("input[name=anonymous]:checked").serialize(),
-url     : "functions/userprivacyupdater.php",
-success : function (anonymous)
-		  {   $('#privacy').removeClass('formfields');   
-		    $('#privacy').load(document.URL +  ' #privacy');  
-		     },
-		error   : function ( xhr )
-		  { alert( "error" );
-		  }
-		  
-} );
- return false;	
-}
-</script>
 
 
-<script type="text/javascript">
-$(document).ready(function() {
-// Tooltip only Text
-$('.masterTooltip').hover(function(){
-        // Hover over code
-        var title = $(this).attr('title');
-        $(this).data('tipText', title).removeAttr('title');
-        $('<p class="tooltip"></p>')
-        .text(title)
-        .appendTo('body')
-        .fadeIn('slow');
-}, function() {
-        // Hover out code
-        $(this).attr('title', $(this).data('tipText'));
-        $('.tooltip').remove();
-}).mousemove(function(e) {
-        var mousex = e.pageX + 20; //Get X coordinates
-        var mousey = e.pageY + 10; //Get Y coordinates
-        $('.tooltip')
-        .css({ top: mousey, left: mousex })
-});
-});
-</script>
 
-
-</body>
+/body>
 </html>
 <?php
 mysql_free_result($rs_view_message);
