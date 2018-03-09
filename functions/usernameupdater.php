ad2072589cc558be36e321c9e01401b12427ea25
nawissor <shutterman@iwanross.co.za>
2018-03-09 09:59:15 +0200

    update emails to production email address

--- functions/usernameupdater.php
+++ functions/usernameupdater.php
@@ -1,5 +1,9 @@
 <?php require_once('../Connections/killjoy.php'); ?>
 <?php
+ob_start();
+if (!isset($_SESSION)) {
+session_start();
+}
 if (!function_exists("GetSQLValueString")) {
 function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
 {
@@ -31,27 +35,88 @@ function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDe
 }
 }
 
-if ((isset($_POST["txt_city"])) && ($_POST["txt_city"] != "")) {
-  $updateSQL = sprintf("UPDATE tbl_address SET city=%s WHERE sessionid=%s",
-                       GetSQLValueString($_POST['txt_city'], "text"),
-                       GetSQLValueString($_POST['txt_sesseyed'], "text"));
+$colname_rs_user_details = "-1";
+if (isset($_SESSION['kj_username'])) {
+  $colname_rs_user_details = $_SESSION['kj_username'];
+}
+mysql_select_db($database_killjoy, $killjoy);
+$query_rs_user_details = sprintf("SELECT * FROM social_users WHERE g_email = %s", GetSQLValueString($colname_rs_user_details, "text"));
+$rs_user_details = mysql_query($query_rs_user_details, $killjoy) or die(mysql_error());
+$row_rs_user_details = mysql_fetch_assoc($rs_user_details);
+$totalRows_rs_user_details = mysql_num_rows($rs_user_details);
+
+
+
+if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "update")) {
+$register_success_url = "index.php";
+$password = password_hash($password, PASSWORD_BCRYPT);
+  $updateSQL = sprintf("UPDATE social_users SET g_name=%s WHERE g_email = %s",
+                       GetSQLValueString($_POST['g_name'], "text"),                      
+					   GetSQLValueString($_SESSION['kj_username'], "text"));
 
   mysql_select_db($database_killjoy, $killjoy);
   $Result1 = mysql_query($updateSQL, $killjoy) or die(mysql_error());
+  
+date_default_timezone_set('Africa/Johannesburg');
+$date = date('d-m-Y H:i:s');
+$time = new DateTime($date);
+$date = $time->format('d-m-Y');
+$time = $time->format('H:i:s'); 
+  
+require('phpmailer-master/class.phpmailer.php');
+include('phpmailer-master/class.smtp.php');
+$name = $row_rs_user_details['g_name'];
+$email = $row_rs_user_details['g_email'];
+$email_1 = "friends@killjoy.co.za";
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->Host = "killjoy.co.za";
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = "ssl";
+$mail->Username = "friends@killjoy.co.za";
+$mail->Password = "806Ppe##44VX";
+$mail->Port = "465";
+$mail->SetFrom('friends@killjoy.co.za', 'Killjoy Community');
+$mail->AddReplyTo("friends@killjoy.co.za","Killjoy Community");
+$message = "<html><head><style type='text/css'>
+a:link {
+text-decoration: none;
+}
+a:visited {
+text-decoration: none;
+}
+a:hover {
+text-decoration: none;
+}
+a:active {
+text-decoration: none;
+}
+body,td,th {
+font-family: Tahoma, Geneva, sans-serif;
+font-size: 14px;
+}
+body {
+background-repeat: no-repeat;
+margin-left:50px;
+}
+</style></head><body>Dear ". $name ."<br><br>Your <a href='https://www.killjoy.co.za'>killjoy.co.za</a> profile has been updated<br><br>If you made any changes to your email address or passwords, <a href='https://www.killjoy.co.za/admin/index.php'>please sign in </a>again to ensure the new changes are reflected.<br><br>The updates to your killjoy.co.za account was sent from: <a href='mailto:$email'>$email</a> on $date at $time<br><br>If this was not you, please let us know by sending an email to: <a href='mailto:friends@killjoy.co.za'>Killjoy</a><br><br><br><br>Thank you, the Killjoy Community: https://www.killjoy.co.za<br><br><font size='2'>If you received this email by mistake, pleace let us know: <a href='mailto:friends@killjoy.co.za'>Killjoy</a></font><br><br></body></html>";
+$mail->Subject    = "killjojy.co.za Profile Updated";
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$body = "$message\r\n";
+$body = wordwrap($body, 70, "\r\n");
+$mail->MsgHTML($body);
+$address = $email;
+$mail->AddAddress($address, "Killjoy");
+$mail->AddCC($email_1, "Killjoy");
+if(!$mail->Send()) {
+echo "Mailer Error: " . $mail->ErrorInfo;
 }
 
+header('Location: ' . filter_var($register_success_url  , FILTER_SANITIZE_URL));
 
-
-
-$colname_get_address = "-1";
-if (isset($_SESSION['sessionid'])) {
-  $colname_get_address = $_SESSION['sessionid'];
 }
-mysql_select_db($database_killjoy, $killjoy);
-$query_get_address = sprintf("SELECT * FROM tbl_address WHERE sessionid = %s", GetSQLValueString($colname_get_address, "text"));
-$get_address = mysql_query($query_get_address, $killjoy) or die(mysql_error());
-$row_get_address = mysql_fetch_assoc($get_address);
-$totalRows_get_address = mysql_num_rows($get_address);
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -66,5 +131,7 @@ $totalRows_get_address = mysql_num_rows($get_address);
 
 </html>
 <?php
+mysql_free_result($rs_user_details);
+
 mysql_free_result($get_address);
 ?>
--- myprofile.php
+++ myprofile.php
@@ -102,75 +102,6 @@ return $randomString;
 $sessionid = generateRandomString();
 
 	
-if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "update")) {
-$register_success_url = "index.php";
-$password = password_hash($password, PASSWORD_BCRYPT);
-  $updateSQL = sprintf("UPDATE social_users SET g_name=%s WHERE g_email = %s",
-                       GetSQLValueString($_POST['g_name'], "text"),                      
-					   GetSQLValueString($_SESSION['kj_username'], "text"));
-
-  mysql_select_db($database_killjoy, $killjoy);
-  $Result1 = mysql_query($updateSQL, $killjoy) or die(mysql_error());
-  
-date_default_timezone_set('Africa/Johannesburg');
-$date = date('d-m-Y H:i:s');
-$time = new DateTime($date);
-$date = $time->format('d-m-Y');
-$time = $time->format('H:i:s'); 
-  
-require('phpmailer-master/class.phpmailer.php');
-include('phpmailer-master/class.smtp.php');
-$name = $_POST['g_name'];
-$email = $_POST['g_email'];
-$email_1 = "iwan@wellingtoncomputers.co.za";
-$mail = new PHPMailer();
-$mail->IsSMTP();
-$mail->Host = "killjoy.co.za";
-$mail->SMTPAuth = true;
-$mail->SMTPSecure = "ssl";
-$mail->Username = "friends@killjoy.co.za";
-$mail->Password = "806Ppe##44VX";
-$mail->Port = "465";
-$mail->SetFrom('friends@killjoy.co.za', 'Killjoy Community');
-$mail->AddReplyTo("friends@killjoy.co.za","Killjoy Community");
-$message = "<html><head><style type='text/css'>
-a:link {
-text-decoration: none;
-}
-a:visited {
-text-decoration: none;
-}
-a:hover {
-text-decoration: none;
-}
-a:active {
-text-decoration: none;
-}
-body,td,th {
-font-family: Tahoma, Geneva, sans-serif;
-font-size: 14px;
-}
-body {
-background-repeat: no-repeat;
-margin-left:50px;
-}
-</style></head><body>Dear ". $name ."<br><br>Your <a href='https://www.killjoy.co.za'>killjoy.co.za</a> profile has been updated<br><br>If you made any changes to your email address or passwords, <a href='https://www.killjoy.co.za/admin/index.php'>please sign in </a>again to ensure the new changes are reflected.<br><br>The updates to your killjoy.co.za account was sent from: <a href='mailto:$email'>$email</a> on $date at $time<br><br>If this was not you, please let us know by sending an email to: <a href='mailto:friends@killjoy.co.za'>Killjoy</a><br><br><br><br>Thank you, the Killjoy Community: https://www.killjoy.co.za<br><br><font size='2'>If you received this email by mistake, pleace let us know: <a href='mailto:friends@killjoy.co.za'>Killjoy</a></font><br><br></body></html>";
-$mail->Subject    = "killjojy.co.za Profile Updated";
-$headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$body = "$message\r\n";
-$body = wordwrap($body, 70, "\r\n");
-$mail->MsgHTML($body);
-$address = $email;
-$mail->AddAddress($address, "Killjoy");
-$mail->AddCC($email_1, "Killjoy");
-if(!$mail->Send()) {
-echo "Mailer Error: " . $mail->ErrorInfo;
-}
-
-header('Location: ' . filter_var($register_success_url  , FILTER_SANITIZE_URL));
-
-}
 
 $colname_show_error = "-1";
 if (isset($_SESSION['sessionid'])) {
@@ -231,7 +162,7 @@ $image_id = $row_rs_profile_image['image_id'];?>
 <?php } ?>
 </div>
   <div class="fieldlabels" id="fieldlabels">Your name:</div>
-  <div class="formfields" id="formfields"><span id="sprytextfield1">
+  <div class="formfields" id="membername"><span id="sprytextfield1">
     <label>
       <input name="g_name" type="text" class="inputfields" id="g_name" value="<?php echo $row_rs_member_profile['g_name']; ?>" />
     </label>
@@ -391,11 +322,11 @@ success : function (anonymous)
 <script type="text/javascript">
  function member_name( anonymous ) 
 { $.ajax( { type    : "POST",
-data: $("input[name=anonymous]:checked").serialize(),
+data:  { "txt_name" : $("#g_name").val(), "MM_insert" : $("#MM_insert").val()},
 url     : "functions/usernameupdater.php",
 success : function (anonymous)
-		  {   $('#privacy').removeClass('formfields');   
-		    $('#privacy').load(document.URL +  ' #privacy');  
+		  {   $('#membername').removeClass('formfields');   
+		    $('#membername').load(document.URL +  ' #membername');  
 		     },
 		error   : function ( xhr )
 		  { alert( "error" );
