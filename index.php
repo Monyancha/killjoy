36dab371dacb8f40c55b539bb45a58d2509d8e42
nawissor <shutterman@iwanross.co.za>
2018-03-12 09:17:14 +0200

    reverting changes to index page

--- index.php
+++ index.php
@@ -5,12 +5,6 @@ ob_start();
 if (!isset($_SESSION)) {
 session_start();
 }
-
-$isanchor = NULL;
-
-if(isset($_COOKIE['is_anchor'])) {
-	$isanchor = $_COOKIE['is_anchor'];
-}
 header("Cache-Control: no-cache, must-revalidate");
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 function generateRandomString($length = 24) {
@@ -332,10 +326,7 @@ span.stars span {
   </div>
   <div class="latestheader"><h2>Latest Reviews</h2></div>
   <?php do { $sessionid = filter_var($row_rs_latest_reviews['propsession'], FILTER_SANITIZE_SPECIAL_CHARS);?>
-  <?php if ($isanchor != NULL) { // Show if recordset not empty ?>
-    <a title="property rental reviews" name="photo" id="photo"></a>
-    <?php } // Show if recordset not empty ?>
-<a onclick="my_button('<?php echo $sessionid ?>')" class="masterTooltip" title="view the tenants experience of <?php echo $row_rs_latest_reviews['streetnumber']; ?> <?php echo $row_rs_latest_reviews['streetname']; ?> <?php echo $row_rs_latest_reviews['city']; ?>" href="viewer.php?tarsus=<?php echo $captcha?>&claw=<?php echo $sessionid ?>&alula=<?php echo $smith ?>"><div class="latestreviews" id="latestreviews">    
+  <a class="masterTooltip" title="view the tenants experience of <?php echo $row_rs_latest_reviews['streetnumber']; ?> <?php echo $row_rs_latest_reviews['streetname']; ?> <?php echo $row_rs_latest_reviews['city']; ?>" href="viewer.php?tarsus=<?php echo $captcha?>&claw=<?php echo $sessionid ?>&alula=<?php echo $smith ?>"><div class="latestreviews" id="latestreviews">    
       <div class="propertyimagecontainer" id="propertyimagecontainer"><img src="<?php echo $row_rs_latest_reviews['propertyImage']; ?>" alt="killjoy property rental reviews and advice" class="propertyimage" /></div>
       <div class="addressbox" id="addressbox"><address><?php echo $row_rs_latest_reviews['streetnumber']; ?><br /><?php echo $row_rs_latest_reviews['streetname']; ?><br /><?php echo $row_rs_latest_reviews['city']; ?></address></div>
       <div class="ratingbox">Rating: <span class="stars" id="stars"><?php echo $row_rs_latest_reviews['Avgrating']; ?></span><?php echo $row_rs_latest_reviews['Avgrating']; ?></div>
@@ -465,6 +456,7 @@ $('.masterTooltip').hover(function(){
 });
 });
 </script>
+
 
 </body>
 </html>
--- viewer.php
+++ viewer.php
@@ -118,6 +118,11 @@ href: '#viewreviews',
 modal: false,
  'afterClose'  : function() {			   
  location.href ="index.php";		
+  var toScroll = $(".fancybox-inner").find("#photo").offset().top - 35;
+            $(".fancybox-inner").animate({
+                scrollTop: toScroll
+            }, 1000);
+ 
 		 
  },
  
