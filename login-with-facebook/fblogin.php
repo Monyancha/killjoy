d9fc25134dd8527fc9bc8fb0eb6f835e24f40409
nawissor <shutterman@iwanross.co.za>
2018-03-15 08:07:09 +0200

    fore some reason facebook sign in stopped working again

--- login-with-facebook/fblogin.php
+++ login-with-facebook/fblogin.php
@@ -119,7 +119,9 @@ var getInfo;
   the FB.login() function when clicked.
 -->
 
-<div class="fb-login-button" data-width="200px" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="true"></div>
+<fb:login-button scope="public_profile,email" id="login" onlogin="checkLoginState();">
+</fb:login-button>
+
 
 
 <div id="status" name="status"></div>
