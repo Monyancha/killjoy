f2f566272200cca7657161a36766f27f2b3b584b
nawissor <shutterman@iwanross.co.za>
2018-03-21 09:20:21 +0200

    set city value null onfocus

--- myprofile.php
+++ myprofile.php
@@ -184,12 +184,9 @@ $image_id = $row_rs_profile_image['image_id'];?>
       <label class="switch">
         <input <?php if (!(strcmp($row_rs_member_profile['location_sharing'],1))) {echo "checked=\"checked\"";} ?> type="checkbox" onclick="member_location()" name="location" id="location" value="1">
         <span class="slider round"></span>
-        </label>   
+        </label>
       </a>
-        <div class="locale">
-          <label for="location">City/Town</label>
-          <input name="citytown" type="text" class="city" id="city" value="<?php echo $row_rs_member_profile['City']; ?>" />
-        </div>
+        <div class="locale"><label for="location">City/Town</label><input name="citytown" type="text" class="city" id="city" value="<?php echo $row_rs_member_profile['City']; ?>" /></div>
     <a href="#" id="privacysettings" class="masterTooltip" title="select this option if you wish to remain anonymoys. None of your personal details will appear on reviews or anywhere else on this site" ><span class="toggletext">Remain Anonymous:</span>
       <label class="switch">
         <input <?php if (!(strcmp($row_rs_member_profile['anonymous'],1))) {echo "checked=\"checked\"";} ?> type="checkbox" onclick="member_privacy()" name="anonymous" id="anonymous" value="1">
@@ -208,6 +205,18 @@ $image_id = $row_rs_profile_image['image_id'];?>
 </div>
 <input type="hidden" name="MM_insert" value="update" />
 </form>
+
+
+<script type="text/javascript">
+$(document).ready( function() {
+    $("#city").focus( function() {
+        if ( $(this).val()=="<?php echo $row_rs_member_profile['City']; ?>") {
+            $(this).val('');
+        } 
+    });
+	 });
+</script>
+
 <script type="text/javascript">
 function acceptimage() {
 var data = new FormData();
