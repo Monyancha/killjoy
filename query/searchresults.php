bf0132bb494e596c277c6152e5c387ca14ae2cbd
nawissor <shutterman@iwanross.co.za>
2018-03-27 15:29:59 +0200

    adding record paging

--- query/searchresults.php
+++ query/searchresults.php
@@ -1,6 +1,32 @@
 <?php require_once('../Connections/killjoy.php'); ?>
  <?php
  
+ function generateRandomString($length = 24) {
+    $characters = '0123456789abcdefghijklmnopqrstuvw!@#$%^&^*()';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+$captcha = filter_var(generateRandomString(), FILTER_SANITIZE_SPECIAL_CHARS);
+$captcha = urlencode($captcha);
+
+function generatenewRandomString($length = 24) {
+    $characters = '0123456789abcdefghijklmnopqrstuvw!@#$%^&^*()';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+$smith = filter_var(generateRandomString(), FILTER_SANITIZE_SPECIAL_CHARS);
+$smith = urlencode($smith);
+ 
  $q=$_GET['q'];
 $mysqli=mysqli_connect('localhost','euqjdems_nawisso','N@w!1970','euqjdems_killjoy') or die("Database Error");
 $my_data=mysqli_real_escape_string($mysqli, $_GET['q']);
@@ -36,8 +62,6 @@ function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDe
 }
 }
 
-$currentPage = $_SERVER["PHP_SELF"];
-
 $maxRows_rs_search_results = 10;
 $pageNum_rs_search_results = 0;
 if (isset($_GET['pageNum_rs_search_results'])) {
@@ -46,7 +70,7 @@ if (isset($_GET['pageNum_rs_search_results'])) {
 $startRow_rs_search_results = $pageNum_rs_search_results * $maxRows_rs_search_results;
 
 mysql_select_db($database_killjoy, $killjoy);
-$query_rs_search_results = "SELECT DISTINCT (SELECT COUNT(tbl_address_comments.sessionid) FROM tbl_address_comments WHERE tbl_address_comments.sessionid = tbl_address.sessionid) AS reviewCount, city as city, str_number as strNumber, IFNULL(tbl_propertyimages.image_url,'https://www.killjoy.co.za/images/icons/house-outline-bg.png') AS propertyImage, street_name AS Street, tbl_address.sessionid As id, city as Town FROM tbl_address LEFT JOIN tbl_address_comments ON tbl_address_comments.sessionid = tbl_address.sessionid LEFT JOIN tbl_propertyimages ON tbl_propertyimages.sessionid = tbl_address.sessionid LEFT JOIN tbl_approved ON tbl_approved.address_comment_id = tbl_address_comments.id WHERE tbl_address.street_name LIKE '%$my_data%' OR tbl_address.str_number LIKE '%$my_data%' AND tbl_approved.is_approved = '1' GROUP BY tbl_address.sessionid ORDER BY Street ASC";
+$query_rs_search_results = "SELECT DISTINCT (SELECT COUNT(tbl_address_comments.sessionid) FROM tbl_address_comments WHERE tbl_address_comments.sessionid = tbl_address.sessionid) AS reviewCount, city as city, str_number as strNumber, IFNULL(tbl_propertyimages.image_url,'https://www.killjoy.co.za/images/icons/house-outline-bg.png') AS propertyImage, IFNULL(tbl_approved.is_approved,'0') as Status, street_name AS Street, tbl_address.sessionid As id, city as Town FROM tbl_address LEFT JOIN tbl_address_comments ON tbl_address_comments.sessionid = tbl_address.sessionid LEFT JOIN tbl_propertyimages ON tbl_propertyimages.sessionid = tbl_address.sessionid LEFT JOIN tbl_approved ON tbl_approved.address_comment_id = tbl_address_comments.id WHERE tbl_address.street_name LIKE '%$my_data%' OR tbl_address.str_number LIKE '%$my_data%' OR city like '%$my_data%' GROUP BY tbl_address.sessionid ORDER BY Street ASC";
 $query_limit_rs_search_results = sprintf("%s LIMIT %d, %d", $query_rs_search_results, $startRow_rs_search_results, $maxRows_rs_search_results);
 $rs_search_results = mysql_query($query_limit_rs_search_results, $killjoy) or die(mysql_error());
 $row_rs_search_results = mysql_fetch_assoc($rs_search_results);
@@ -58,50 +82,6 @@ if (isset($_GET['totalRows_rs_search_results'])) {
   $totalRows_rs_search_results = mysql_num_rows($all_rs_search_results);
 }
 $totalPages_rs_search_results = ceil($totalRows_rs_search_results/$maxRows_rs_search_results)-1;
-
-$queryString_rs_search_results = "";
-if (!empty($_SERVER['QUERY_STRING'])) {
-  $params = explode("&", $_SERVER['QUERY_STRING']);
-  $newParams = array();
-  foreach ($params as $param) {
-    if (stristr($param, "pageNum_rs_search_results") == false && 
-        stristr($param, "totalRows_rs_search_results") == false) {
-      array_push($newParams, $param);
-    }
-  }
-  if (count($newParams) != 0) {
-    $queryString_rs_search_results = "&" . htmlentities(implode("&", $newParams));
-  }
-}
-$queryString_rs_search_results = sprintf("&totalRows_rs_search_results=%d%s", $totalRows_rs_search_results, $queryString_rs_search_results);
-
-function generateRandomString($length = 24) {
-    $characters = '0123456789abcdefghijklmnopqrstuvw!@#$%^&^*()';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
-    }
-    return $randomString;
-}
-
-$captcha = filter_var(generateRandomString(), FILTER_SANITIZE_SPECIAL_CHARS);
-$captcha = urlencode($captcha);
-
-function generatenewRandomString($length = 24) {
-    $characters = '0123456789abcdefghijklmnopqrstuvw!@#$%^&^*()';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
-    }
-    return $randomString;
-}
-
-$smith = filter_var(generateRandomString(), FILTER_SANITIZE_SPECIAL_CHARS);
-$smith = urlencode($smith);
-
-
 
 
 ?>
@@ -140,28 +120,10 @@ $smith = urlencode($smith);
 <body>
 <div class="formcontainer">
 <div class="formheader">Showing results for <span class="mydata"><?php echo $my_data ?></span></div>
-
-<?php do { ?>
-  <div class="results"><div class="marker"><span class='icon-map-marker'></span></div><img class="image" src="<?php echo $row_rs_search_results['propertyImage']; ?>"  alt="search results image"/><div class="addressfield"><?php echo $row_rs_search_results['strNumber'] ?>, <?php echo $row_rs_search_results['Street'] ?>, <?php echo $row_rs_search_results['city'] ?></div></div>
-  <?php } while ($row_rs_search_results = mysql_fetch_assoc($rs_search_results)); ?>
-  <table border="0">
-    <tr>
-      <td><?php if ($pageNum_rs_search_results > 0) { // Show if not first page ?>
-          <a href="<?php printf("%s?pageNum_rs_search_results=%d%s", $currentPage, 0, $queryString_rs_search_results); ?>">First</a>
-          <?php } // Show if not first page ?></td>
-      <td><?php if ($pageNum_rs_search_results > 0) { // Show if not first page ?>
-          <a href="<?php printf("%s?pageNum_rs_search_results=%d%s", $currentPage, max(0, $pageNum_rs_search_results - 1), $queryString_rs_search_results); ?>">Previous</a>
-          <?php } // Show if not first page ?></td>
-      <td><?php if ($pageNum_rs_search_results < $totalPages_rs_search_results) { // Show if not last page ?>
-          <a href="<?php printf("%s?pageNum_rs_search_results=%d%s", $currentPage, min($totalPages_rs_search_results, $pageNum_rs_search_results + 1), $queryString_rs_search_results); ?>">Next</a>
-          <?php } // Show if not last page ?></td>
-      <td><?php if ($pageNum_rs_search_results < $totalPages_rs_search_results) { // Show if not last page ?>
-          <a href="<?php printf("%s?pageNum_rs_search_results=%d%s", $currentPage, $totalPages_rs_search_results, $queryString_rs_search_results); ?>">Last</a>
-          <?php } // Show if not last page ?></td>
-    </tr>
-  </table>
+  <?php do { ?>
+    <div class="results"><div class="marker"><span class='icon-map-marker'></span></div><img class="image" src="<?php echo $row_rs_search_results['propertyImage']; ?>"  alt="search results image"/><div class="addressfield"><?php echo $row_rs_search_results['strNumber'] ?>, <?php echo $row_rs_search_results['Street'] ?>, <?php echo $row_rs_search_results['city'] ?></div></div>
+    <?php } while ($row_rs_search_results = mysql_fetch_assoc($rs_search_results)); ?>
 </div>
-
 
 </body>
 </html>
