d6ef13519b4b97b7cc61915a658c7803d19f2124
nawissor <shutterman@iwanross.co.za>
2018-03-28 09:23:56 +0200

    trying to add transparent rating images

---
+++ query/untitled.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style type="text/css">
+.whitehouse:before {
+	font-family: icomoon;
+	content: "\f015";
+	font-size: 160px;
+	color: #56B2D7;
+	
+	
+}
+.whitehouse {
+	background-color: #C8C9CB;
+	height: 160px;
+	width: 160px;
+	vertical-align: middle;
+	text-align: center;
+	margin-top: 250px;
+	margin-left: 250px;
+	
+}
+
+</style>
+<link href="../iconmoon/style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<img src="../images/stars/frey_square.png" class="whitehouse" width="160" height="160" />
+</body>
+</html>
