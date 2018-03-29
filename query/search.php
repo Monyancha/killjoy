4236d14f924259c3b9a4802f7530125f6bacde8b
nawissor <shutterman@iwanross.co.za>
2018-03-29 16:35:29 +0200

    query adjusted to get best and worst ratings

--- query/search.php
+++ query/search.php
@@ -1,5 +1,10 @@
 <?php require_once('../Connections/killjoy.php'); ?>
 <?php
+
+$q=$_GET['q'];
+$mysqli=mysqli_connect('localhost','euqjdems_nawisso','N@w!1970','euqjdems_killjoy') or die("Database Error");
+$my_data=mysqli_real_escape_string($mysqli, $_GET['q']);
+
 if (!function_exists("GetSQLValueString")) {
 function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
 {
@@ -32,7 +37,7 @@ function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDe
 }
 
 mysql_select_db($database_killjoy, $killjoy);
-$query_rs_structured_markup = "SELECT tbl_address_comments.sessionid as id, tbl_address.str_number as strNumber, tbl_address.street_name AS Street, tbl_address.city as city, (SELECT COUNT(tbl_approved.sessionid) FROM tbl_approved WHERE tbl_approved.sessionid = tbl_address_comments.sessionid AND tbl_approved.is_approved=1) AS reviewCount, IFNULL(tbl_propertyimages.image_url,'images/icons/house-outline-bg.png') AS propertyImage, tbl_address_comments.rating_date as ratingDate, IFNULL(tbl_approved.is_approved,'0') as Status, ROUND(AVG(tbl_address_rating.rating_value),1) AS avgRating, (SELECT COUNT(tbl_address_rating.address_comment_id) FROM tbl_address_comments WHERE tbl_address_rating.address_comment_id = tbl_address_comments.id) as ratingCount FROM `euqjdems_killjoy`.`tbl_address` LEFT JOIN tbl_address_comments ON tbl_address_comments.sessionid = tbl_address.sessionid LEFT JOIN tbl_approved ON tbl_approved.address_comment_id = tbl_address_comments.id LEFT JOIN tbl_propertyimages ON tbl_propertyimages.sessionid = tbl_address.sessionid LEFT JOIN tbl_address_rating ON tbl_address_rating.address_comment_id = tbl_address_comments.id LEFT JOIN tbl_addressindex ON tbl_addressindex.sessionid=tbl_address.sessionid WHERE tbl_addressindex.address LIKE '%$my_data%' GROUP BY tbl_address_comments.sessionid ORDER BY tbl_address_comments.rating_date DESC";
+$query_rs_structured_markup = "SELECT tbl_address_comments.sessionid as id, tbl_address.str_number as strNumber, tbl_address.street_name AS Street, tbl_address.city as city, (SELECT COUNT(tbl_approved.sessionid) FROM tbl_approved WHERE tbl_approved.sessionid = tbl_address_comments.sessionid AND tbl_approved.is_approved=1) AS reviewCount, MIN(tbl_address_rating.rating_value) as worstRating, MAX(tbl_address_rating.rating_value) as bestRating, tbl_address_comments.social_user as reViewer, tbl_address.postal_code as postal_code, tbl_address.province as province, IFNULL(tbl_propertyimages.image_url,'images/icons/house-outline-bg.png') AS propertyImage, tbl_address_comments.rating_date as ratingDate, IFNULL(tbl_approved.is_approved,'0') as Status, ROUND(AVG(tbl_address_rating.rating_value),1) AS avgRating, (SELECT COUNT(tbl_address_rating.address_comment_id) FROM tbl_address_comments WHERE tbl_address_rating.address_comment_id = tbl_address_comments.id) as ratingCount FROM `euqjdems_killjoy`.`tbl_address` LEFT JOIN tbl_address_comments ON tbl_address_comments.sessionid = tbl_address.sessionid LEFT JOIN tbl_approved ON tbl_approved.address_comment_id = tbl_address_comments.id LEFT JOIN tbl_propertyimages ON tbl_propertyimages.sessionid = tbl_address.sessionid LEFT JOIN tbl_address_rating ON tbl_address_rating.address_comment_id = tbl_address_comments.id LEFT JOIN tbl_addressindex ON tbl_addressindex.sessionid=tbl_address.sessionid WHERE tbl_addressindex.address LIKE '%$my_data%' GROUP BY tbl_address_comments.sessionid ORDER BY tbl_address_comments.rating_date DESC";
 $rs_structured_markup = mysql_query($query_rs_structured_markup, $killjoy) or die(mysql_error());
 $row_rs_structured_markup = mysql_fetch_assoc($rs_structured_markup);
 $totalRows_rs_structured_markup = mysql_num_rows($rs_structured_markup);
@@ -62,6 +67,44 @@ $totalRows_rs_structured_markup = mysql_num_rows($rs_structured_markup);
       "image": "https://www.killjoy.co.za/images/icons/search-icon.png"
     }
     }]
+}
+</script>
+ <script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "Residence",
+  "name": "<?php echo $row_rs_structured_review['streetnumber']; ?>, <?php echo $row_rs_structured_review['streetname']; ?>, <?php echo $row_rs_structured_review['city']; ?>, <?php echo $row_rs_structured_review['postal_code']; ?>",
+  "address": {
+    "@type": "PostalAddress",
+    "addressLocality": "<?php echo $row_rs_structured_review['city']; ?>",
+    "addressRegion": "<?php echo $row_rs_structured_review['province']; ?>",
+    "postalCode": "<?php echo $row_rs_structured_review['postal_code']; ?>",
+    "streetAddress": "<?php echo $row_rs_structured_review['streetnumber']; ?>, <?php echo $row_rs_show_review['streetname']; ?>"
+  },
+    
+  
+  "review": [
+    {
+      "@type": "Review",
+      "author": "<?php echo $row_rs_structured_review['social_user']; ?>",
+      "datePublished": "<?php echo $row_rs_structured_review['ratingDate']; ?>",
+      "description": "<?php echo $row_rs_structured_review['comments']; ?>",
+      "name": "<?php echo $row_rs_structured_review['feeling']; ?>",
+      "reviewRating": {
+        "@type": "Rating",
+        "bestRating": "<?php echo $row_rs_structured_review['bestRating']; ?>",
+        "ratingValue": "<?php echo round($row_rs_structured_review['Avgrating'],0); ?>",
+        "worstRating": "<?php echo $row_rs_structured_review['worstRating']; ?>"
+      }
+    }
+  ],
+   "aggregateRating": {
+    "@type": "AggregateRating",
+    "ratingValue": "<?php echo round($row_rs_structured_review['Avgrating'],0); ?>",
+    "reviewCount": "<?php echo $row_rs_structured_review['ratingCount']; ?>"
+  },
+   "photo": "https://www.killjoy.co.za/<?php echo $row_rs_structured_review['propertyImage']; ?>",
+   "image": "https://www.killjoy.co.za/<?php echo $row_rs_structured_review['propertyImage']; ?>"
 }
 </script>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
