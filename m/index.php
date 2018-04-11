b4e39625ea8a919e70f5a20710b6d76142d6ff7b
nawissor <shutterman@iwanross.co.za>
2018-04-11 11:18:20 +0200

    adding iconmoon fonts

--- m/index.php
+++ m/index.php
@@ -5,17 +5,29 @@
 <title>Untitled Document</title>
 <link href="../jquery-mobile/jquery.mobile.theme-1.3.0.min.css" rel="stylesheet" type="text/css">
 <link href="../jquery-mobile/jquery.mobile.structure-1.3.0.min.css" rel="stylesheet" type="text/css">
+<link href="css/gui.css" rel="stylesheet" type="text/css">
 <script src="../jquery-mobile/jquery-1.11.1.min.js"></script>
 <script src="../jquery-mobile/jquery.mobile-1.3.0.min.js"></script>
+<style type='text/css'>
+.ui-page .ui-header {
+    background: #6EADC1 !important;
+	height: 65px;
+}
+	.ui-page .ui-footer {
+    background: #6EADC1 !important;
+}
+</style>
+<link href="iconmoon/style.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
 <div data-role="page" id="index-page">
-  <div class="pop" data-role="header">
+  <div class="header" data-role="header">
     <h1>Killjoy.co.za</h1>
-  </div>
-  <div class="banner" id="banner"></div>
-<div data-role="content"><form>Content</form></div>
+	  <div class="social-user-image"></div>
+    <img class="site-header-logo" src="images/icons/owl-header-white.gif" width="512" height="512" alt=""/>
+     </div>
+ <div data-role="content"><form>Content</form></div>
   <div data-role="footer">
     <h4>Footer</h4>
   </div>
