8f7400cdae11093833e07eabc653520ac3472b31
nawissor <shutterman@iwanross.co.za>
2018-05-10 14:47:36 +0200

    cookies warning div movoed outside help file div

--- index.php
+++ index.php
@@ -529,10 +529,10 @@ span.stars span {
   </div>
 	<div id="searchbox" class="search-box"><div class="search-box-header">Have a question?</div><div class="search-box-container"><form target="new" action="help-and-support/index.php" name="findanswers" id="findanswers"><input placeholder="type a question to find an answer" autofocus class="searchfield" type="search" data-type="search" name="q" id="q"><input type="submit" style="position: absolute; left: -9999px"/></form></div>
   <?php if ($consent == 0) { // Show if recordset empty ?>
-  <div class="cookiewarning" id="cookiewarning">    
-  <div class="cookiemessage" id="cookiemessage">This site uses cookies. By continuing you <a  target="_new" title="View our cookie policy" href="info-centre/cookie-policy.php">agree to our use of cookies</a>.</div><a onClick="my_button('<?php echo $click_time; ?>')" href="#"><div class="gotit">Got it!</div></a></div>   
-
 </div>
+
+ <div class="cookiewarning" id="cookiewarning">    
+  <div class="cookiemessage" id="cookiemessage">This site uses cookies. By continuing you <a  target="_new" title="View our cookie policy" href="info-centre/cookie-policy.php">agree to our use of cookies</a>.</div><a onClick="my_button('<?php echo $click_time; ?>')" href="#"><div class="gotit">Got it!</div></a></div>   
 <?php } // Show if recordset empty ?>
 
 
